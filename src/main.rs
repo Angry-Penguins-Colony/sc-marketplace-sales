75acf4cd8e389351a541541296f9eacd7d43dc7b
TF <theo.farnole+github@gmail.com>
2023-02-09 11:00:33 +0100

    make contract building works

--- src/main.rs
+++ src/main.rs
@@ -2,55 +2,68 @@
 
 multiversx_sc::imports!();
 
-/// An empty contract. To be used as a template when starting a new contract from scratch.
 #[multiversx_sc::contract]
 pub trait EmptyContract {
     #[init]
     fn init(&self) {}
 
-    #[only_owner]
-    #[payable("*")]
-    #[endpoint(createAuction)]
-    fn create_auction(
-        token: TokenIdentifier,
-        nonce: u64,
-        price: BigUint,
-    ) {
-        // let payments = self.call_value().all_esdt_transfers();
-        todo!();
-    }
+    // #[only_owner]
+    // #[payable("*")]
+    // #[endpoint(createAuction)]
+    // fn create_auction(
+    //     &self,
+    //     token: TokenIdentifier,
+    //     nonce: u64,
+    //     price: BigUint,
+    // ) {
+    //     // let payments = self.call_value().all_esdt_transfers();
+    //     todo!();
+    // }
 
-    #[only_owner]
-    #[endpoint(stopAuction)]
-    fn stop_auction(id: u64) {
-        todo!();
-    }
+    // #[only_owner]
+    // #[endpoint(stopAuction)]
+    // fn stop_auction(
+    //     &self, 
+    //     id: u64
+    // ) {
+    //     todo!();
+    // }
 
-    #[only_owner]
-    #[endpoint(retireTokenFromAuction)]
-    fn retire_token_from_auction(id: u64, amount: u64) {
-        todo!();
-    }
+    // #[only_owner]
+    // #[endpoint(retireTokenFromAuction)]
+    // fn retire_token_from_auction(
+    //     &self,
+    //     id: u64, 
+    //     amount: u64
+    // ) {
+    //     todo!();
+    // }
 
-    #[only_owner]
-    #[endpoint(withdrawBalance)]
-    fn withdraw_balance() {
-        todo!();
-    }
+    // #[only_owner]
+    // #[endpoint(withdrawBalance)]
+    // fn withdraw_balance(&self,) {
+    //     todo!();
+    // }
 
-    #[payable("*")]
-    #[endpoint()]
-    fn buy(quantity: u64) {
-        todo!();
-    }
+    // #[payable("*")]
+    // #[endpoint()]
+    // fn buy(
+    //     &self,
+    //     quantity: u64
+    // ) {
+    //     todo!();
+    // }
 
-    #[view(getAuction)]
-    fn get_auction(id: u64) {
-        todo!();
-    }
+    // #[view(getAuction)]
+    // fn get_auction(
+    //     &self,
+    //     id: u64
+    // ) {
+    //     todo!();
+    // }
 
-    #[view(getActiveAuctions)]
-    fn get_active_auctions() {
-        todo!();
-    }
+    // #[view(getActiveAuctions)]
+    // fn get_active_auctions(&self) {
+    //     todo!();
+    // }
 }
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -16,7 +16,7 @@ multiversx_sc_wasm_adapter::allocator!();
 multiversx_sc_wasm_adapter::panic_handler!();
 
 multiversx_sc_wasm_adapter::endpoints! {
-    empty
+    apc_sales
     (
     )
 }
