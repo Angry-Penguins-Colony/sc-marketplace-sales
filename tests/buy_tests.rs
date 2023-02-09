1588f4d7eeb743d5206c23268e13b8b4018d91b7
TF <theo.farnole+github@gmail.com>
2023-02-09 14:37:40 +0100

    ADD buy_fail_if_locked

--- tests/buy_tests.rs
+++ tests/buy_tests.rs
@@ -1,4 +1,4 @@
-use apc_sales::EmptyContract;
+use apc_sales::{EmptyContract, ERR_SALE_IS_NOT_OPENED_YET};
 use multiversx_sc::types::BoxedBytes;
 use multiversx_sc_scenario::rust_biguint;
 
@@ -12,6 +12,35 @@ fn buy_one_successful() {
 #[test]
 fn buy_two_successful() {
     buy_n_succesfully(2);
+}
+
+#[test]
+fn buy_fail_if_locked() {
+    let mut setup = helpers::setup_contract(apc_sales::contract_obj);
+
+    const PRICE: u64 = 50;
+    const QUANTITY: u64 = 1;
+    const START_TIMESTAMP: u64 = 10;
+    const NOW_TIMESTAMP: u64 = 5;
+
+    setup.create_default_auction(PRICE, START_TIMESTAMP, QUANTITY);
+    setup
+        .blockchain_wrapper
+        .set_egld_balance(&setup.user_address, &rust_biguint!(PRICE * QUANTITY));
+
+    setup.blockchain_wrapper.set_block_timestamp(NOW_TIMESTAMP);
+
+    setup
+        .blockchain_wrapper
+        .execute_tx(
+            &setup.user_address,
+            &setup.contract_wrapper,
+            &rust_biguint!(PRICE * QUANTITY),
+            |sc| {
+                sc.buy(QUANTITY);
+            },
+        )
+        .assert_user_error(ERR_SALE_IS_NOT_OPENED_YET);
 }
 
 fn buy_n_succesfully(quantity: u64) {
